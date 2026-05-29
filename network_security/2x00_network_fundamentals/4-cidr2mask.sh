b4726db05765c1142ee6c55bced3c58b9615e03e
aydin827 <13597@holbertonstudents.com>
2026-05-29 19:31:49 +0000

    F

--- network_security/2x00_network_fundamentals/4-cidr2mask.sh
+++ network_security/2x00_network_fundamentals/4-cidr2mask.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for i in 1 2 3 4; do if [ $1 -ge 8 ]; then echo -n 255; elif [ $1 -le 0 ]; then echo -n 0; else echo -n $((256-(2**(8-$1)))); fi; [ $i -lt 4 ] && echo -n .; [ $1 -ge 8 ] && $((1-=8)) >/dev/null || true; done; echo
+for i in 1 2 3 4; do if [ $1 -ge 8 ]; then echo -n 255; elif [ $1 -le 0 ]; then echo -n 0; else echo -n $
