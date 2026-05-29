3770b21b561d2f0c4791d1f60d5b79a34537ea21
aydin827 <13597@holbertonstudents.com>
2026-05-29 19:42:42 +0000

    TASK4

--- network_security/2x00_network_fundamentals/7-host_range.sh
+++ network_security/2x00_network_fundamentals/7-host_range.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-f(){ IFS=. read a b c d<<<$1;echo $((a<<24|b<<16|c<<8|d));};g(){ printf "%d.%d.%d.%d" $(($1>>24&255)) $(($1>>16&255)) $(($1>>8&255)) $(($1&255));};n=$(f "$1");m=$((0xffffffff << (32-$2) & 0xffffffff));g $((n&m+1));printf " - ";g $((n|~m&0xffffffff-1))
+f(){ IFS=. read a b c d<<<$1;echo $((a<<24|b<<16|c<<8|d));};g(){ printf "%d.%d.%d.%d" $(($1>>24&255)) $(($1>>16&255)) $(($1>>8&255)) $(($1&255));};n=$(f "$1");m=$((0xffffffff << (32-$2) & 0xffffffff));g $((n&m+1));printf " - ";g $(((n|~m&0xffffffff)-1))
